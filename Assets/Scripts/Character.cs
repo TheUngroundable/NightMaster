a583933415aa9fe25fe147801714bf5b89bf7d51
TheUngroundable <35199075+TheUngroundable@users.noreply.github.com>
2019-01-26 01:52:36 +0100

    Character Class
    With movement methods

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    // Public Variables (name order by a-Z)
+    public float deadZone = 0.1f;
+    public string horizontalAxis = "Horizontal";
+    public float gravity = 20.0f;
+    public float speed = 2.0f;
+    public string verticalAxis = "Vertical";
 
-    public GameObject light;
-    public boolean light;
+    // Private Variables (name order by a-Z)
+    private CharacterController controller;
+    private Vector3 moveDirection;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        controller = GetComponent<CharacterController>();
+        moveDirection = Vector3.zero;
+        gameObject.transform.position = new Vector3(transform.position.x, 5, transform.position.z);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        Movement();
+    }
+
+    void Movement(){
+        moveDirection = Vector3.zero;
+        if (Mathf.Abs(Input.GetAxis(horizontalAxis)) > deadZone || Mathf.Abs(Input.GetAxis(verticalAxis)) > deadZone){
+            moveDirection = new Vector3(Input.GetAxis(horizontalAxis), 0.0f, Input.GetAxis(verticalAxis));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
